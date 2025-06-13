51e9a49ade2783248437cda88b44fb533759c1eb
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2025-06-13 13:18:41 +0300

    added raw header

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,50 @@
 <template>
-  <div id="app">
-    <transition name="fade">
-      <IntroScreen v-if="showIntro" />
-    </transition>
+    <div id="app">
+        <transition name="fade">
+            <IntroScreen v-if="showIntro" />
+        </transition>
 
-    <div class="main-content" v-show="!showIntro">
-      <LoginForm />
+        <div class="main-content" v-show="!showIntro">
+            <Header />
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
 import IntroScreen from "./components/IntroScreen.vue";
 import LoginForm from "./components/LoginForm.vue";
+import Header from "./components/Header.vue";
 
 const showIntro = ref(true);
 
 onMounted(() => {
-  setTimeout(() => {
-    showIntro.value = false;
-  }, 4000);
+    setTimeout(() => {
+        showIntro.value = false;
+    }, 4000);
 });
 </script>
 
 <style>
 body {
-  margin: 0;
-  padding: 0;
-  background-color: #121212;
-  font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #121212;
+    font-family: Arial, sans-serif;
 }
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 1s;
+    transition: opacity 1s;
 }
 .fade-enter,
 .fade-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 
 .main-content {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
+    min-height: 100vh;
+    display: flex;
+    color: white;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,60 @@
 <template>
-  <div>
-    <div class="header">
-      <span>Header</span>
+    <div style="width: 100%">
+        <div class="custom-header">
+            <div class="avatar">
+                <img src="../assets/avatar.jpg  " alt="Avatar" />
+            </div>
+            <span class="custom-title">WorkDay Pusher</span>
+            <span href="/" class="away_refs">Главная</span>
+            <span href class="away_refs">Профиль</span>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup></script>
 
 <style scoped>
-.header {
-  background: black;
-  padding-top: 0px;
-  border-color: black;
+.avatar {
+    width: 50px; /* Размер круга */
+    height: 50px; /* Должен совпадать с width */
+    margin: 20px;
+    border-radius: 50%; /* Делает круг */
+    overflow: hidden; /* Обрезает изображение по кругу */
+    border: 2px solid #fff; /* Белая рамка (опционально) */
+}
+
+.away_refs {
+    margin: 20px 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.away_refs:hover {
+    color: grey;
+    border-bottom: 2px solid grey;
+}
+
+.avatar img {
+    width: 200%;
+    height: 200%;
+    object-fit: cover; /* Обрезает изображение */
+    object-position: center; /* Фокусируется на центре */
+}
+
+.custom-title {
+    width: 65%;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    font-size: 26px;
+}
+
+.custom-header {
+    display: flex;
+    flex-direction: row;
+    padding: 20px;
+    background: black;
+    border-color: black;
 }
 </style>
