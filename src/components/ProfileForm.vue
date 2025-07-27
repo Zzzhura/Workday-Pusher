beca00b686ae6ad5900c52bb8e60298237c7eab3
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2025-07-27 17:18:19 +0300

    added new profile form with primevue component

--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -1,73 +1,35 @@
 <template>
-    <div
-        style="
-            background-color: white;
-            border: 2px solid grey;
-            margin: 20px;
-            border-radius: 10px;
-            display: flex;
-            flex-direction: row;
-            width: 50%;
-        "
-    >
-        <div class="avatar">
-            <img src="../assets/avatar.jpg  " alt="Avatar" />
-        </div>
-        <div class="info">
-            <span class="user-info">Иван Журавлев</span>
-            <span class="dolzhfun">Должность: Fullstack разработчик</span>
-            <span style="font-size: 14px; font-weight: 500"
-                >Организация: "ООО Альфа-Интегратор"</span
-            >
-            <span style="font-size: 14px; font-weight: 500"
-                >Местоположение: Россия, Санкт-Петербург</span
-            >
-        </div>
-    </div>
+  <div class="profile-container">
+    <Avatar
+        image="src/assets/avatar.jpg"
+        class="avatar"
+        size="xlarge"
+        shape="circle"
+    />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Avatar from 'primevue/avatar';
+</script>
 
-<style>
+<style scoped>
+.profile-container {
+  width: 360px;
+  height: 289px;
+  margin-left: 35px;
+  border-radius: 20px;
+  border: 3px solid white;
+  background-color: rgb(0 0 0 / 0.5);
+  color: red;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding-top: 20px;
+}
+
 .avatar {
-    width: 100px;
-    height: 100px;
-    margin: 60px;
-    margin-right: 20px;
-    margin-left: 90px;
-    border-radius: 50%;
-    overflow: hidden;
-    border: 2px solid grey;
-}
-
-.avatar img {
-    width: auto;
-    height: 100%;
-    object-fit: cover;
-    object-position: center;
-}
-
-.info {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    color: black;
-    font-size: 16px;
-    padding: 20px 0;
-    margin-bottom: 110px;
-    line-height: 1.5;
-}
-
-.user-info {
-    font-weight: bold;
-    font-size: 22px;
-}
-
-.dolzhfun {
-    border-radius: 5px;
-    padding: 3px 5px;
-    background-color: #d9d9d9;
-    font-size: 12px;
+  width: 80px;
+  height: 80px;
 }
 </style>
